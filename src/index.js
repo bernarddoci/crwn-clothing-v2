f191eb57cab484e523bf5cd55e83a6b94f35cdd8
bernarddoci <bernard.doci@gmail.com>
2022-08-10 23:44:51 +0200

    Stripe payment added

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import App from './App';
-import { PersistGate } from 'redux-persist/integration/react';
-import reportWebVitals from './reportWebVitals'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import { store, persistor } from './store/store';
+import { BrowserRouter } from "react-router-dom";
 
-import './index.scss';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
+          <Elements stripe={stripePromise}>
             <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,19 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { setIsCartOpen } from '../../store/cart/cart.actions';
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import { useDispatch, useSelector } from "react-redux";
+import { setIsCartOpen } from "../../store/cart/cart.actions";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
-import { 
+import {
   CheckoutContainer,
   CheckoutHeader,
   HeaderBlock,
-  Total
-} from './checkout.styles';
+  Total,
+} from "./checkout.styles";
 
-const labels = ['Product', 'Description', 'Quantity', 'Price', 'Remove'];
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
+const labels = ["Product", "Description", "Quantity", "Price", "Remove"];
 
 const Checkout = () => {
   const dispatch = useDispatch();
@@ -22,31 +27,28 @@ const Checkout = () => {
 
   useEffect(() => {
     dispatch(setIsCartOpen(false));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <CheckoutContainer>
       <CheckoutHeader>
-        {
-          labels.map(label => 
-            <HeaderBlock key={label}>
-              <span>{label}</span>
-            </HeaderBlock>
-          )
-        }
+        {labels.map((label) => (
+          <HeaderBlock key={label}>
+            <span>{label}</span>
+          </HeaderBlock>
+        ))}
       </CheckoutHeader>
-      {
-      cartItems.length && cartItems.map(cartItem => <CheckoutItem 
-        key={cartItem.id} 
-        cartItem={cartItem} 
-        />)
-      }
+      {cartItems.length &&
+        cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
       <Total>
         <span>TOTAL: ${cartTotal}</span>
       </Total>
+      <PaymentForm />
     </CheckoutContainer>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
